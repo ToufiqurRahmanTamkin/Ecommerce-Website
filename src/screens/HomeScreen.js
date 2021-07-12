fa6d4e969d2ca77b7b1ab3c817e091bbb986160e
Toufiqur Rahman Tamkin <tamkin22rahman@gmail.com>
2021-07-12 17:55:36 +0600

    refactore codes

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -7,6 +7,7 @@ export default function HomeScreen() {
     <div>
       <div className="row center">
         {data.products.map((product) => (
+            
           <Product key={product._id} product={product}></Product>
         ))}
       </div>
